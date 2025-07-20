5a00f8ee6211c380d699805a9f2ff2fed6e3b0cb
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-07-20 16:23:24 +0330

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -11,13 +11,18 @@ conn = snowflake.connector.connect(
     password=st.secrets["snowflake"]["password"],
     account=st.secrets["snowflake"]["account"],
     warehouse="SNOWFLAKE_LEARNING_WH",
-    database="YOUR_DATABASE",
-    schema="YOUR_SCHEMA"
+    database="AXELAR",
+    schema="PUBLIC"
 )
 
 @st.cache_data
 def load_data():
-    query = "SELECT * FROM YOUR_TABLE LIMIT 1000;"
+    query = "select date_trunc('month',block_timestamp) as "Date", count(distinct tx_id) as "TXs Count", 
+tx_succeeded as "TX Success"
+from AXELAR.CORE.FACT_TRANSACTIONS
+where block_timestamp::date>='2022-01-01'
+group by 1,3
+order by 1"
     return pd.read_sql(query, conn)
 
 df = load_data()
