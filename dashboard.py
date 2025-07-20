45ec3f5c88df3d4b183fceab9a62df93a01dd86b
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-07-20 16:25:54 +0330

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -17,12 +17,12 @@ conn = snowflake.connector.connect(
 
 @st.cache_data
 def load_data():
-    query = "select date_trunc('month',block_timestamp) as "Date", count(distinct tx_id) as "TXs Count", 
+    query = """select date_trunc('month',block_timestamp) as "Date", count(distinct tx_id) as "TXs Count", 
 tx_succeeded as "TX Success"
 from AXELAR.CORE.FACT_TRANSACTIONS
 where block_timestamp::date>='2022-01-01'
 group by 1,3
-order by 1"
+order by 1"""
     return pd.read_sql(query, conn)
 
 df = load_data()
